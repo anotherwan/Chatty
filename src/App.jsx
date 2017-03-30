ea85cff81c512dd62c54c85d3f5a62d980f68e83
anotherwan <itsanotherwan@gmail.com>
2017-03-30 13:14:38 -0400

    changed to dynamic port and host

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ class App extends Component {
 
   componentDidMount() {
 
-    this.socket = new WebSocket(config.HOST,config.PORT);
+    this.socket = new WebSocket(`${config.HOST}${config.PORT}`);
     // const HOST = location.origin.replace(/^http/, 'ws');
     // this.socket = new WebSocket(HOST);
     // this.socket.onopen = () => conn.send('Connected')
